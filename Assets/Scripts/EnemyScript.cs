35651801ef19f8e818bc49f714e9702c4dcd61de
Josh Worley <josher131@hotmail.com>
2020-09-26 15:53:52 -0400

    Added walls, enemy and coin prefabs

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+
+    public float startX;
+    public float startY;
+
+    public float endX;
+    public float endY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Transform>().position = new Vector3(0, 0, 0);
+    }
+}
