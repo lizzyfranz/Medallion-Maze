b8425479a9ff88cc0f22ea1393a52f8bec1a4a08
Josh Worley <josher131@hotmail.com>
2020-09-29 21:45:58 -0400

    Player can move

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    Rigidbody2D rbody;
+    public int maxSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = maxSpeed * Input.GetAxis("Horizontal");
+        float y = maxSpeed * Input.GetAxis("Vertical");
+        rbody.velocity = new Vector2(x, y);
     }
 }
